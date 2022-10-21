874657909ccc444cbdb51041137c4478603e3a15
stevewanash <stevewanash1@gmail.com>
2022-10-21 04:25:44 +0300

    removed header guards and test folder

--- main.h
+++ main.h
@@ -1,6 +1,1 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
-
 int _printf(const char *format, ...);
-
-#endif
--- test/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-int main()
-{
-    _printf("Hello World");
-    //printf("Hello % World");
-    return (0);
-}
