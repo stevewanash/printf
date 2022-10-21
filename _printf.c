10c9b4fdaef258f64a8d8d0e6c948575a9b9facc
stevewanash <stevewanash1@gmail.com>
2022-10-21 04:16:58 +0300

    first printf version

---
+++ _printf.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...)
+{
+    int i = 0;
+
+    char *j;
+
+    j = malloc(100);
+
+    /*char k = 0;
+
+    char *l;
+
+    l = malloc(100);
+
+
+    va_list ap;
+
+    va_start(ap, format);
+    
+    while (k < format)
+    {
+        l = va_arg(ap, int);*/
+
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == ' ')
+            {
+                j[i] = format[i];
+
+                i++;                
+                /*if (format [i + 1] == 'c')
+                {
+                    
+                }
+                else if (format [i + 1] == 's')
+                {
+                    
+                }
+                if (format [i + 1] == 'd')
+                {
+                    
+                }
+                if (format [i + 1] == 'f')
+                {
+                    
+                }
+                if (format [i + 1] == 'd')
+                {
+                    
+                }*/
+            }
+            else if (!(format [i + 1]))
+            {
+                return (-1);
+            }
+            /*else
+            {
+                write (1, '%', 1);
+            }*/
+        }
+        /*else if (format [i] == '\\')
+        {
+            if (format [i + 1] != ' ' && format [i + 1] != '\0' )
+            {
+                if (format [i + 1] == 'n')
+                {
+
+                }
+                else if (format [i + 1] == 't')
+                {
+                    
+                }
+                /*else
+                {
+                
+                }
+            }
+        }*/
+        else
+        {
+            j[i] = format[i];
+
+            i++;
+        }
+    }
+
+    write(1, j, i);
+
+    free(j);
+
+    return (i);
+}
---
+++ test/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+    _printf("Hello World");
+    //printf("Hello % World");
+    return (0);
+}
